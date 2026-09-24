9781f06558f900295f00b529de4893e99bcb4e60
MadfixIsMe <p.lukac47@gmail.com>
2026-09-24 14:43:03 +0200

    úloha 12 SQL dotaz

--- superstore/database_queries.sql
+++ superstore/database_queries.sql
@@ -60,3 +60,8 @@ FROM customers c
 JOIN orders o ON c.customer_id = o.customer_id
 GROUP BY c.region;
 
+--úloha 12
+SELECT c.region, COUNT(CASE WHEN o.sales > 1000 THEN 1 END) AS high_value, COUNT(CASE WHEN o.sales <= 1000 THEN 1 END) AS low_value
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+GROUP BY c.region;
