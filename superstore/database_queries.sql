964de46b81849cc96a45bec365c7440c929511ae
MadfixIsMe <p.lukac47@gmail.com>
2026-09-24 14:06:22 +0200

    úloha 3 SQL dotaz

--- superstore/database_queries.sql
+++ superstore/database_queries.sql
@@ -3,3 +3,8 @@ FROM orders o
 JOIN customers c ON o.customer_id = c.customer_id
 WHERE o.sales > 500
 ORDER BY o.sales DESC;
+
+SELECT o.order_id, c.customer_name, p.category, o.sales
+FROM orders o
+JOIN customers c ON o.customer_id = c.customer_id
+JOIN products p ON o.product_id = p.product_id;
