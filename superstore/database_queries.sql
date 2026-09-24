a0db8fadac9ad454d0f8c16b12e27d44c8011fdb
MadfixIsMe <p.lukac47@gmail.com>
2026-09-24 14:18:58 +0200

    úloha 5 SQL dotaz

--- superstore/database_queries.sql
+++ superstore/database_queries.sql
@@ -13,3 +13,9 @@ SELECT c.region, SUM(o.sales) AS celk_hodnota
 FROM customers c
 LEFT JOIN orders o ON c.customer_id = o.customer_id
 GROUP BY c.region;   
+
+SELECT p.product_name, SUM(o.sales) as celk_hodnota
+FROM products p
+LEFT JOIN orders o ON p.product_id = o.product_id
+GROUP BY p.product_name;
+
